e426d4e33c519d44136041305b72e7428ef532fe
denQTER <den100pts@gmail.com>
2025-02-27 04:29:24 +0200

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,7 +1,13 @@
+import logging
 import random
 import os
 from telegram import Update
-from telegram.ext import Application, CommandHandler, CallbackContext
+from telegram.ext import Application, CommandHandler, CallbackContext, CallbackQueryHandler
+
+# Настроим логирование
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 # Путь к папке с фото
 PHOTO_FOLDER = "photos"
@@ -15,8 +21,12 @@ async def send_random_photo(update: Update, context: CallbackContext):
 
     photo_path = os.path.join(PHOTO_FOLDER, random.choice(photo_list))  
     with open(photo_path, "rb") as photo:
-        # Отправляем фото с текстом в одном сообщении
-        await update.message.reply_photo(photo, caption="Спасибо что вызвали волосатое чмо, можете полюбоваться мной перед тем как плюнуть в монитор")
+        await update.message.reply_photo(photo)
+
+# Функция обработки ошибок
+def error(update: Update, context: CallbackContext):
+    """Логирование ошибок, вызвавших исключение"""
+    logger.warning('Update "%s" caused error "%s"', update, context.error)
 
 # Основной код
 def main():
@@ -24,6 +34,9 @@ def main():
 
     app = Application.builder().token(TOKEN).build()
     app.add_handler(CommandHandler("swinngger_volosati_pidor", send_random_photo))
+
+    # Добавляем обработчик ошибок
+    app.add_error_handler(error)
 
     print("Бот запущен...")
     app.run_polling()
